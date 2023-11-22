3be3ebc6f75bbea85fe40803a796bf134d1e4610
Matias Alzugaray <alzugaray1997@gmail.com>
2023-11-22 19:26:20 -0300

    subiendo primera pre entrega

---
+++ src/routes/carts.router.js
@@ -0,0 +1,129 @@
+// carts.router.js
+import { Router } from 'express';
+import { products } from './products.router.js';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+
+const cartRouter = Router();
+
+const cartsFilePath = 'carts.json';
+
+const readCartsFromFile = () => {
+  try {
+    const data = fs.readFileSync(cartsFilePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
+
+const writeCartsToFile = (carts) => {
+  fs.writeFileSync(cartsFilePath, JSON.stringify(carts, null, 2), 'utf-8');
+};
+
+const carts = readCartsFromFile();
+
+cartRouter.get('/', (req, res) => {
+  res.json({
+    carts: carts,
+  });
+});
+
+cartRouter.post('/', (req, res) => {
+  const { idProduct } = req.body;
+
+  if (!idProduct) {
+    return res
+      .status(400)
+      .json({
+        error: 'Debe ingresar el producto que quiere agregar al carrito',
+      });
+  }
+
+  // Verifica si el producto existe en la lista de productos
+  const product = products.find((el) => el.id === idProduct);
+
+  if (!product) {
+    return res.status(404).json({ error: 'Producto no encontrado.' });
+  }
+
+  // Crea una nueva cart con un ID único y agrega el producto directamente
+  const cart = {
+    idCart: uuidv4(),
+    products: [product],
+  };
+
+  carts.push(cart);
+
+  writeCartsToFile(carts);
+
+  res.json({
+    message: 'El carrito se creó con éxito',
+    idCart: cart.idCart,
+  });
+});
+
+cartRouter.post('/:cid/product/:pid', (req, res) => {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+  
+    const cart = carts.find((el) => el.idCart === cid);
+  
+    if (!cart) {
+      console.log('Carrito no encontrado.');
+      return res.status(400).json({ error: `El carrito con el id ${cid} no existe` });
+    }
+  
+    const product = products.find((el) => el.id === pid);
+  
+    if (!product) {
+      console.log('Producto no encontrado.');
+      return res.status(400).json({ error: `El producto con el id ${pid} no existe` });
+    }
+
+  const existingProduct = cart.products.find(
+    (item) => item.id === pid
+  );
+
+  if (existingProduct) {
+    existingProduct.quantity += 1;
+  } else {
+    const newProduct = {
+      id: uuidv4(),
+      ...product,
+      quantity: 1,
+    };
+
+    cart.products.push(newProduct);
+  }
+
+  writeCartsToFile(carts);
+
+  res.json({
+    message: `Producto con el id ${pid} agregado al carrito con el id ${cid}`,
+    cart: cart,
+  });
+});
+
+cartRouter.get('/:cid', (req, res) => {
+  const cid = req.params.cid;
+
+  if (!cid) {
+    return res.status(400).json({ error: 'Error al hacer la búsqueda' });
+  }
+
+  const carrito = carts.find((el) => el.idCart === cid);
+
+  if (!carrito) {
+    return res
+      .status(400)
+      .json({ error: `El carrito con el id ${cid} no existe` });
+  }
+
+  res.json({
+    message: 'Carrito encontrado',
+    cart: carrito,
+  });
+});
+
+export default cartRouter;
---
+++ src/routes/products.router.js
@@ -0,0 +1,127 @@
+// products.router.js
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+const productRouter = Router();
+
+// Función para leer productos desde el archivo "productos.json"
+const readProductsFromFile = () => {
+  try {
+    const data = fs.readFileSync("productos.json", "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
+
+// Función para escribir productos en el archivo "productos.json"
+const writeProductsToFile = (products) => {
+  fs.writeFileSync("productos.json", JSON.stringify(products, null, 2), "utf-8");
+};
+
+export const products = readProductsFromFile();
+
+productRouter.get("/", (req, res) => {
+  const limit = req.query.limit;
+
+  if (!limit) return res.json({ products });
+
+  if (products.length < limit)
+    return res.status(500).json({ error: "No existen tantos productos" });
+
+  const productLimit = products.slice(0, limit);
+
+  res.json({
+    productos: productLimit,
+  });
+});
+
+productRouter.post("/", (req, res) => {
+  const { title, description, code, price, stock, category, thumbnails } =
+    req.body;
+
+  if (
+    !title ||
+    !description ||
+    !code ||
+    price === undefined ||
+    stock === undefined ||
+    !category
+  ) {
+    return res.status(400).json({
+      error:
+        "Todos los campos obligatorios, excepto 'thumbnails', deben ser proporcionados",
+    });
+  }
+
+  if (
+    typeof code !== "string" ||
+    typeof price !== "number" ||
+    typeof stock !== "number" ||
+    typeof category !== "string"
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Tipo de datos incorrecto para uno o más campos" });
+  }
+
+  const product = {
+    id: uuidv4(),
+    title: title,
+    description: description,
+    code: code,
+    price: price,
+    status: true,
+    stock: stock,
+    category: category,
+    thumbnails: thumbnails || [],
+  };
+
+  products.push(product);
+
+  // Actualizar el archivo "productos.json" con los productos actualizados
+  writeProductsToFile(products);
+
+  res.json({
+    message: "Producto agregado con éxito",
+  });
+});
+
+productRouter.put("/:pid", (req, res) => {
+  const pid = req.params.pid;
+
+  const index = products.findIndex((el) => el.id === pid);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  products[index] = { ...products[index], ...req.body };
+
+  // Actualizar el archivo "productos.json" con los productos actualizados
+  writeProductsToFile(products);
+
+  res.json({
+    message: "Producto actualizado",
+  });
+});
+
+productRouter.delete("/:pid", (req, res) => {
+  const pid = req.params.pid;
+
+  const index = products.findIndex((el) => el.id === pid);
+
+  if (index === -1)
+    return res.status(400).json({ error: "El producto a eliminar no existe" });
+
+  products.splice(index, 1);
+
+  writeProductsToFile(products);
+
+  res.json({
+    message: "Producto eliminado con éxito",
+  });
+});
+
+export default productRouter;
